c1e1d3a5e7a1a4d32be8ad21091b8c88ea4f8e97
Sindre Sorhus <sindresorhus@gmail.com>
2015-05-07 00:12:16 +0200

    less is more

--- index.js
+++ index.js
@@ -2,12 +2,9 @@
 var through = require('through2');
 var belch = require('belch');
 
-module.exports = function (opts) {
-  opts = opts || {};
-
+module.exports = function () {
   return through.obj(function (file, enc, cb) {
     belch();
-    this.push(file);
-    cb();
+    cb(null, file);
   });
 };
